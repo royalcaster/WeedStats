a5bb21ab13d998d6ef021ac9332531fe059e76db
Gabriel Pechstein <gabriel.pechstein@schneeberg.km3.de>
2024-08-16 14:08:09 +0200

    sync

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,59 +1,105 @@
+//--------------------------------------------------------------------------
+//  IMPORTS
+//--------------------------------------------------------------------------
 // React
-import { useContext, useEffect } from 'react';
+import { useContext, useEffect, useState, useCallback } from 'react';
 import 'react-native-reanimated';
-import { Text } from 'react-native';
+import { View } from 'react-native';
+
 // Expo
 import { useFonts } from 'expo-font';
+import * as Font from 'expo-font';
 import { Stack } from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
+import { getLocales } from 'expo-localization';
 
 // Hooks
 import { useColorScheme } from '@/hooks/useColorScheme';
 import { ThemeContext } from '@/contexts/ThemeContext';
 import { Colors } from '@/constants/Colors';
 
+//Constants
+import translations from '../constants/languages';
+
+//3rd Party
+import { I18n } from 'i18n-js';
+
+//--------------------------------------------------------------------------
+//  SETUP
+//--------------------------------------------------------------------------
 SplashScreen.preventAutoHideAsync();
 
+// Set the key-value pairs for the different languages you want to support.
+const i18n = new I18n(translations);
+
+// Set the locale once at the beginning of your app.
+i18n.locale = getLocales()[0].languageCode ?? 'en';
+// When a value is missing from a language it'll fall back to another language with the key present.
+i18n.enableFallback = true;
+
+//--------------------------------------------------------------------------
+//  COMPONENT START
+//--------------------------------------------------------------------------
 export default function RootLayout() {
 
-
-  const colorScheme = useColorScheme();
+  //States
+  const [appIsReady, setAppIsReady] = useState(false);
 
   useEffect(() => {
-    console.log(colorScheme);
-  },[]);
+    async function prepare() {
+      try {
+        // Load fonts
+        Font.loadAsync('PoppinsBold', require('../assets/fonts/Poppins-Bold.ttf'));
+
+        // Artificially delay for two seconds to simulate a slow loading
+        // experience. Please remove this if you copy and paste the code!
+        await new Promise(resolve => setTimeout(resolve, 2000));
+      } catch (e) {
+        console.warn(e);
+      } finally {
+        // Tell the application to render
+        setAppIsReady(true);
+      }
+    }
+
+    prepare();
+  }, []);
 
   // Theme & Font
   const theme = useContext(ThemeContext);
-  const [loaded] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
-  });
+  
 
-
-  useEffect(() => {
-    if (loaded) {
-      SplashScreen.hideAsync();
+  const onLayoutRootView = useCallback(async () => {
+    if (appIsReady) {
+      // This tells the splash screen to hide immediately! If we call this after
+      // `setAppIsReady`, then we may see a blank screen while the app is
+      // loading its initial state and rendering its first pixels. So instead,
+      // we hide the splash screen once we know the root view has already
+      // performed layout.
+      await SplashScreen.hideAsync();
     }
-  }, [loaded]);
+  }, [appIsReady]);
 
-  if (!loaded) {
+  if (!appIsReady) {
     return null;
   }
 
   return (
-    <ThemeContext.Provider value={colorScheme === "dark" ? Colors.dark : Colors.light}>
-      <Stack
-      screenOptions={{
-        headerStyle: {
-          backgroundColor: theme.background1,
-        },
-        headerTintColor: '#fff',
-        headerTitleStyle: {
-          fontWeight: 'bold',
-        },
-      }}>
-        <Stack.Screen name="index" options={{title: 'Sign in'}}/>
-      </Stack>
-    </ThemeContext.Provider>
+    <View style={{flex: 1}} onLayout={onLayoutRootView}>
+      <ThemeContext.Provider value={Colors.dark}>
+        <Stack
+        screenOptions={{
+          headerStyle: {
+            backgroundColor: theme.background1,
+          },
+          headerTintColor: 'white',
+          headerTitleStyle: {
+            fontFamily: 'PoppinsBold',
+          },
+        }}>
+          <Stack.Screen name="index" options={{title: i18n.t('signin')}}/>
+        </Stack>
+      </ThemeContext.Provider>
+    </View>
   );
 }
---
+++ constants/languages.js
@@ -0,0 +1,11 @@
+const translations = {
+    en: { 
+        welcome: 'Welcome',
+        signin: 'Sign in',
+    },
+    de: { 
+        welcome: 'Willkommen',
+        signin: 'Anmelden',
+    },
+  };
+export default translations;
